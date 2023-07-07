af279097d3c6cd39416cad4827fa4bb0bb5e4f30
webdev <webdev181011@gmail.com>
2023-07-07 11:10:50 +0900

    init

---
+++ dist/_nuxt/90f0a26.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{337:function(n,t,l){"use strict";l.r(t);var o=l(81),component=Object(o.a)({},(function(){return(0,this._self._c)("p",[this._v("Good luck!")])}),[],!1,null,null,null);t.default=component.exports}}]);
